c5efb46aa927160b83892d3ee0ead47f6a8d5041
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-20 15:57:39 +0530

    Added method to obtain value

--- src/Components/Square/Square.js
+++ src/Components/Square/Square.js
@@ -4,12 +4,21 @@ import './Square.css'
 class Square extends React.Component {
   constructor(props) {
     super(props)
+    this.assignValue = this.assignValue.bind(this)
+  }
+
+  assignValue() {
+    let value = this.props.value
+                ? () => null
+                : (
+                    this.props.getSquareValue(this.props.index)
+                  )
   }
 
   render() {
     return (
-      <div className="squareContainer">
-        {this.props.value}
+      <div className="squareContainer" title="Click to add a random number" onClick={this.assignValue}>
+        {(this.props.value) ? this.props.value : ''}
       </div>
     )
   }
