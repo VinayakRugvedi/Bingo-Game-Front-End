6d016e255c1ff415f4dc7e37bfec805caf9c0566
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-20 10:53:23 +0530

    Set up the initial point js file to render Board component

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Board from './Components/Board/Board.js'
+
+ReactDOM.render(
+  <Board/>,
+  document.getElementById('root')
+)
