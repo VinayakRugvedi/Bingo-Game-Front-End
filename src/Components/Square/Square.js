2320d77fd5b8b0be6f4c8f7ff39d658bfa1d3d0a
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-26 11:54:45 +0530

    Remove states and deal with props

--- src/Components/Square/Square.js
+++ src/Components/Square/Square.js
@@ -4,18 +4,12 @@ import './Square.css'
 class Square extends React.Component {
   constructor (props) {
     super (props)
-    this.state = {
-      squareSet : false,
-      valueSpoken : false
-    }
     this.assignValue = this.assignValue.bind(this)
   }
 
   assignValue () {
     if (!this.props.square.value) {
-      this.setState({
-        squareSet : true
-      }, () => this.props.updateSquareValue(this.props.square.position))
+      this.props.updateSquareValue(this.props.square.position)
     }
   }
 
@@ -24,14 +18,14 @@ class Square extends React.Component {
       <div
         style={{backgroundColor : this.props.square.color}}
         className="squareContainer"
-        title="Click to add a random number"
-        onClick={(this.state.squareSet && this.props.boardSet)
+        title={this.props.square.value ? 'Click to mark this number' : 'Click to assign a number'}
+        onClick={(this.props.square.value && this.props.boardSet)
                   ? () => {
                     if(!this.props.square.marked && this.props.myTurn)
                     this.props.talkToServer(this.props.square.position)
                   }
                   : this.assignValue}>
-          {(this.props.square.value) ? this.props.square.value : ''}
+        <div className="valueHolder">{(this.props.square.value) ? this.props.square.value : ''}</div>
       </div>
     )
   }
